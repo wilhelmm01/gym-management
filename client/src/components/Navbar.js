ef62d1918e4fc0e849a6932308c65d5e6dc5639f
Cody Webb <webbcody36@gmail.com>
2022-04-05 13:56:27 -0400

    minor changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,7 +15,7 @@ const AppNavbar = () => {
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            Our Gym
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar'>
