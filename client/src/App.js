6ff72cd95e40a81dfc3b7b647ce5957b32476d69
Your Name <matt14w@gmail.com>
2022-04-06 18:59:47 -0400

    matt's branch commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+// import SearchBooks from './pages/SearchBooks';
+// import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,9 +10,14 @@ function App() {
       <>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          {/* <Route exact path='/' component={SearchBooks} /> */}
+          {/* <Route exact path='/saved' component={SavedBooks} /> */}
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/contact" component={Contact}/>
+          <Route exact path="/reviews" component={Reviews}/>
+          <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/trainers" component={Trainers}/>
+          {/* <Route render={() => <h1 className='display-2'>Wrong page!</h1>} /> */}
         </Switch>
       </>
     </Router>
