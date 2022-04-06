9aa3f8e0968dfe3c3b82e02bb222732a4f6d9dbd
Cody Webb <webbcody36@gmail.com>
2022-04-06 07:26:09 -0400

    added trainers page

---
+++ client/src/components/Trainers.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+function Trainers() {
+    return (
+      <div class="trainers" id="trainers">
+          <h1 class="heading"> <span>Our Trainers</span></h1>
+
+          <div class="box-container">
+
+              <div class="box">
+                  <img src="" alt=""></img>
+                  <div class="content">
+                      <h3>Trainer 1</h3>
+                      <div class="share">
+                      <a href="#" class="fab fa-facebook-f"></a>
+                      <a href="#" class="fab fa-twitter"></a>
+                      <a href="#" class="fab fa-pinterest"></a>
+                      <a href="#" class="fab fa-linkedin"></a>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="box">
+                  <img src="" alt=""></img>
+                  <div class="content">
+                      <h3>Trainer 2</h3>
+                      <div class="share">
+                      <a href="#" class="fab fa-facebook-f"></a>
+                      <a href="#" class="fab fa-twitter"></a>
+                      <a href="#" class="fab fa-pinterest"></a>
+                      <a href="#" class="fab fa-linkedin"></a>
+                </div>
+          </div>
+      </div>
+
+      <div class="box">
+                  <img src="" alt=""></img>
+                  <div class="content">
+                      <h3>Trainer 3</h3>
+                      <div class="share">
+                      <a href="#" class="fab fa-facebook-f"></a>
+                      <a href="#" class="fab fa-twitter"></a>
+                      <a href="#" class="fab fa-pinterest"></a>
+                      <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>    
+        <div class="box">
+                  <img src="" alt=""></img>
+                  <div class="content">
+                      <h3>Trainer 2</h3>
+                      <div class="share">
+                      <a href="#" class="fab fa-facebook-f"></a>
+                      <a href="#" class="fab fa-twitter"></a>
+                      <a href="#" class="fab fa-pinterest"></a>
+                      <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>    
+    )
+}
+
+export default Trainers();
