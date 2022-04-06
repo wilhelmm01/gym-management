ba0b17471520b37b4c88dd4778aedfc23963bfd2
Cody Webb <webbcody36@gmail.com>
2022-04-06 09:40:15 -0400

    added routes for trainer data

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const trainerRoutes = require('./trainer-routes');
 
+router.use('/trainers', trainerRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
