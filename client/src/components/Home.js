0b36b7ea997de9fd7c61543168e1de1da8cb5576
Your Name <matt14w@gmail.com>
2022-04-11 20:27:51 -0400

    code cleanup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,54 +1,49 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-// import LoginForm from '../components/LoginForm';
-// import SignupForm from '../components/SignupForm';
-// import {Link} from 'react-router-dom';
+
 
 
 const Login = () => {
     
     return (
 
-    <body>
-        <div>
-         <h1>Login Page</h1>
+    
+
+<body>
+<div>
+        {/* <div class="row"> */}
+        <div className="container">
+            {/* <Navbar/> */}
         </div>
-    </body>
+            <div class="column">
+                <div class="card1" >
+                    <h2>
+                        Membership
+                    </h2>
+                    <p class="card-text">
+                        Checkout your membership information
+                    </p>
+                </div>
+                <div class="card2" >
+                    <h2>
+                        Settings
+                    </h2>
+                    <p class="card-text">
+                        Take a look at your account options
+                    </p>
+                </div>
+                <div class="card3" >
+                    <h2>
+                        Trainer
+                    </h2>
+                    <p class="card-text">
+                        Checkout all the information about you and your trainer
+                    </p>
+                </div>
+            </div>
+        </div>
 
-
-// <div>
-//         {/* <div class="row"> */}
-//         <div className="container">
-//             {/* <Navbar/> */}
-//         </div>
-//             <div class="column">
-//                 <div class="card1" style="width: 18rem;">
-//                     <h2>
-//                         Membership
-//                     </h2>
-//                     <p class="card-text">
-//                         Checkout your membership information
-//                     </p>
-//                 </div>
-//                 <div class="card2" style="width: 18rem;">
-//                     <h2>
-//                         Settings
-//                     </h2>
-//                     <p class="card-text">
-//                         Take a look at your account options
-//                     </p>
-//                 </div>
-//                 <div class="card3" style="width: 18rem;">
-//                     <h2>
-//                         Trainer
-//                     </h2>
-//                     <p class="card-text">
-//                         Checkout all the information about you and your trainer
-//                     </p>
-//                 </div>
-//             </div>
-//         </div>
-// </div>
+</body>
     );
 };
 
